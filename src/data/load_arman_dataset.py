6520dfdb77b5b3bfc8e49eee3d7fb9fa8d3c7b17
MohammadHosseini <sayedmh50@gmail.com>
2022-11-06 14:51:24 +0330

    read_file class

---
+++ src/data/load_arman_dataset.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 28 15:09:18 2022
+
+@author: mohammad
+"""
+
+
+train_dir='/home/mohammad/Desktop/prj/PersianNER/data/ArmanPersoNERCorpus.txt'
+
+
+class Read_file():
+    def __init__(self, path):
+        self.path=path
+        
+    def read_data (self):
+        
+        f = open(self.path, encoding="utf-8")
+   
+        sentence = []
+        all_sentence=[]
+        labels = []
+        all_labels=[]
+        sent_num = 1
+        all_words=[]
+        all_words_labels=[]
+
+        lines=f.readlines()
+    
+        for line in lines:
+            temp=line.split()
+            try:
+            
+                if not temp:
+                    _list=[]
+                    _list.append(sent_num)
+
+                    _list.append(' '.join(sentence))
+
+                    _list.append(' '.join(labels))
+                    
+                    all_sentence.append([' '.join(sentence)])
+                    all_labels.append([' '.join(labels)])
+                    sent_num += 1           
+                    sentence = []
+                    labels = []
+                
+                    if sent_num % 100 == 0:
+                        print('sent_num=', sent_num)
+                    
+                
+                
+                if len(temp)==2:
+                    word = temp[0]
+                    label = temp[1]
+                
+                
+                    sentence.append(word)
+                    labels.append(label.replace('\n', ''))
+                
+            
+                    all_words.append(word)
+                    all_words_labels.append(label)
+
+                
+            except:
+                print(line)
+        return all_sentence, all_labels, all_words, all_words_labels
