34a0c75350bd483be3336ba4de9476c348250d9b
ImMohammadHosseini <sayedmh50@gmail.com>
2023-04-18 12:39:41 +0330

    add read_data method and change dir

---
+++ src/read_data/load_arman_dataset.py
@@ -0,0 +1,30 @@
+
+"""
+
+"""
+
+
+
+def read_data (self, path):
+    f = open(self.path, encoding="utf-8")
+    sentence=[]; all_sentence=[]; labels=[]; all_labels=[]; all_words=[]
+    all_words_labels=[]; sent_num = 1
+    lines=f.readlines()
+    for line in lines:
+        temp=line.split()
+        try:
+            if not temp:
+                _list=[]; _list.append(sent_num)
+                _list.append(' '.join(sentence)); _list.append(' '.join(labels))
+                all_sentence.append([' '.join(sentence)]); all_labels.append([' '.join(labels)])
+                sent_num += 1; sentence = []; labels = []
+                if sent_num % 100 == 0:
+                    print('sent_num=', sent_num)
+
+            if len(temp)==2:
+                word = temp[0]; label = temp[1]
+                sentence.append(word); labels.append(label.replace('\n', ''))
+                all_words.append(word); all_words_labels.append(label)
+        except:
+            print(line)
+    return all_sentence, all_labels, all_words, all_words_labels
